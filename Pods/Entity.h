5a6722b205278b6cab12fd3f4e2695c75d85a99c
miwasa <mtontonis@gmail.com>
2014-09-17 18:12:00 +0900

    fix entity

---
+++ Pods/Entity.h
@@ -0,0 +1,19 @@
+//
+//  Entity.h
+//  Pods
+//
+//  Created by Masaru Iwasa on 2014/09/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Scanitems : NSManagedObject
+
+@property (nonatomic, retain) NSString * names;
+@property (nonatomic, retain) NSNumber * prices;
+@property (nonatomic, retain) NSData * prodacts;
+
+@end
---
+++ Pods/Entity.m
@@ -0,0 +1,18 @@
+//
+//  Entity.m
+//  Pods
+//
+//  Created by Masaru Iwasa on 2014/09/16.
+//
+//
+
+#import "Entity.h"
+
+
+@implementation Scanitems
+
+@dynamic names;
+@dynamic prices;
+@dynamic prodacts;
+
+@end
